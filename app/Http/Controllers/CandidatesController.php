7c4ba09aaf2662b2f22bf2ea70b1f31dee05c464
Aeon Tanvir <aeontanvir@gmail.com>
2018-07-25 14:33:16 +0600

    Candidate create

--- app/Http/Controllers/CandidatesController.php
+++ app/Http/Controllers/CandidatesController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Candidate;
+use App\Area;
+use App\Zone;
+use App\Party;
 use Illuminate\Http\Request;
 
 class CandidatesController extends Controller
@@ -25,13 +28,58 @@ class CandidatesController extends Controller
 
     public function create()
     {
-        //
+        $areas = Area::all();
+        $zones = Zone::all(); 
+        $parties = Party::all();
+        return view('candidates.create', 
+                [
+                    'areas'=> $areas,
+                    'zones'=> $zones,
+                    'parties'=> $parties
+                ]
+            );
     }
 
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'nid' => 'required',
+            'name' => 'required',
+            'father_name' => 'required',
+            'mother_name' => 'required',
+            'occupation' => 'required',
+            'date_of_birth' => 'required',
+            'profile_photo'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'finger_print'  => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'area_id' => 'required',
+            'zone_id' => 'required',
+            'party_id' => 'required'
+        ]);
+
+
+        $input['nid'] = $request->nid;
+        $input['name'] = $request->name;
+        $input['father_name'] = $request->father_name;
+        $input['mother_name'] = $request->mother_name;
+        $input['occupation'] = $request->occupation;
+        $input['date_of_birth'] = $request->date_of_birth;
+        $input['area_id'] = $request->area_id;
+        $input['zone_id'] = $request->zone_id;
+        $input['party_id'] = $request->party_id;
+
+        $input['profile_photo'] = time().'pp.'.$request->profile_photo->getClientOriginalExtension();
+        $request->profile_photo->move(public_path('images/candidates'), $input['profile_photo']);
+
+        $input['finger_print'] = time().'.'.$request->finger_print->getClientOriginalExtension();
+        $request->finger_print->move(public_path('images/candidates'), $input['finger_print']);
+
+        $candidate = Candidate::create($input);
+        if($candidate){
+            return redirect()->route('candidates.show', ['party'=> $candidate->id])
+            ->with('success' , 'Candidate registration successfully');
+        }
+        return back()->withInput()->with('errors', 'Error registration new Candidate!');
     }
 
 
--- resources/views/candidates/create.blade.php
+++ resources/views/candidates/create.blade.php
@@ -2,37 +2,100 @@
 <div class="container">
     <div class="card">
         <div class="card-header">
-            <h2 class="card-title float-left">Party Registration</h2>
+            <h2 class="card-title float-left">Candidate Registration</h2>
         </div>
         <div class="card-body">
 
 
             <form
                 method="POST"
-                action="{{route('parties.store')}}"
+                action="{{route('candidates.store')}}"
                 enctype="multipart/form-data">
 
-
                 {!! csrf_field() !!} 
-                <div class="form-group">
-                    <label for="title">Party Title</label>
-                    <input type="text" name="title" class="form-control" id="title" placeholder="Party Title">
+                
+                
+                <div class="row">
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label for="nid">National ID</label>
+                            <input type="text" name="nid" class="form-control" id="nid" placeholder="NID">
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label for="party_id">Party</label>
+                            <select class="form-control" name="party_id" id="party_id">
+                                @foreach($parties as $party)
+                                    <option value="{{$party->id}}">{{$party->title}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="form-group">
-                    <label for="short">Short Title</label>
-                    <input type="text" name="short" class="form-control" id="short" placeholder="Short Title">
+                    <label for="name">Candidate Fullname</label>
+                    <input type="text" name="name" class="form-control" id="name" placeholder="Name" autocomplete="off">
                 </div>
 
                 <div class="form-group">
-                    <label for="description">Short Description</label>
-                    <textarea type="text" name="description" class="form-control" id="short"></textarea>
+                    <label for="father_name">Father Name</label>
+                    <input type="text" name="father_name" class="form-control" id="father_name" placeholder="Father Name">
+                </div>
+                <div class="form-group">
+                    <label for="mother_name">Mother Name</label>
+                    <input type="text" name="mother_name" class="form-control" id="mother_name" placeholder="Mother Name">
                 </div>
 
                 <div class="form-group">
-                    <label for="party_symbol">Party Symbol</label>
-                    <input type="file" name="image" id="party_symbol" class="form-control">
+                    <label for="occupation">Occupation</label>
+                    <input type="text" name="occupation" class="form-control" id="occupation" placeholder="Occupation">
                 </div>
+
+                <div class="form-group">
+                    <label for="date_of_birth">Date of Birth</label>
+                    <input type="date" name="date_of_birth" class="form-control" id="date_of_birth">
+                </div>
+
+                <div class="row">
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label for="area_id">Area</label>
+                            <select class="form-control" name="area_id" id="area_id">
+                                @foreach($areas as $area)
+                                    <option value="{{$area->id}}">{{$area->district}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label for="zone_id">Area</label>
+                            <select class="form-control" name="zone_id" id="zone_id">
+                                @foreach($zones as $zone)
+                                    <option value="{{$zone->id}}">{{$zone->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label for="profile_photo">Profile Photo</label>
+                            <input type="file" name="profile_photo" id="profile_photo" class="form-control">
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <div class="form-group">
+                            <label for="finger_print">Finger Print Scan</label>
+                            <input type="file" name="finger_print" id="finger_print" class="form-control">
+                        </div>
+                    </div>
+                </div>
+
 
                 <button type="submit" class="btn btn-primary float-right">Register</button>
 
